35ec89a063f5b32bcc5e856213687c8eeecf2026
NehuenZ12 <aldonehuen123@gmail.com>
2026-06-13 17:23:42 -0300

    DocumentoDriver

--- CCTV/src/modelo/DocumentoDriver.java
+++ CCTV/src/modelo/DocumentoDriver.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
 /**
  *
  * @author Nehuen
  */
+
 public class DocumentoDriver {
-    
+
+    private String descripcion;
+    private String version;
+
+    public DocumentoDriver() {
+    }
+
+    public DocumentoDriver(String descripcion, String version) {
+        this.descripcion = descripcion;
+        this.version = version;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
 }
