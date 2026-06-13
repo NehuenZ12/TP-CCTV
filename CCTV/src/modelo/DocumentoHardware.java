fa5462d304dfbf11db8ddc2baf2550e608a2b8fe
NehuenZ12 <aldonehuen123@gmail.com>
2026-06-13 17:22:51 -0300

    DocumentoHardware

---
+++ CCTV/src/modelo/DocumentoHardware.java
@@ -0,0 +1,26 @@
+package modelo;
+
+/**
+ *
+ * @author Nehuen
+ */
+public class DocumentoHardware {
+
+    private String descripcion;
+
+    public DocumentoHardware() {
+    }
+
+    public DocumentoHardware(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
